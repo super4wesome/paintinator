dd2c0bbe4d54a72fa3513586268aa4f37d4ce1f6
Michael Grupp <grupp@magazino.eu>
2019-09-07 14:56:18 +0200

    teleop WIP

--- motortest/motortest.ino
+++ motortest/motortest.ino
@@ -12,11 +12,13 @@ void setup() {
   digitalWrite(enPin,LOW);
   
 }
+
+
 void loop() {
   
   digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
   // Makes 200 pulses for making one full cycle rotation
-  for(int x = 0; x < 400; x++) {
+  for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin,HIGH); 
     delayMicroseconds(500); 
     digitalWrite(stepPin,LOW); 
@@ -26,7 +28,7 @@ void loop() {
 
   digitalWrite(dirPin,LOW); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
-  for(int x = 0; x < 400; x++) {
+  for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin,HIGH);
     delayMicroseconds(500);
     digitalWrite(stepPin,LOW);
