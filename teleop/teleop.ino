2fbab01cdededaddae0e7f13448e687ca2eb2e47
Michael Grupp <grupp@magazino.eu>
2019-09-07 16:17:05 +0200

    homing command added

--- teleop/teleop.ino
+++ teleop/teleop.ino
@@ -11,9 +11,11 @@ const int FULL_PULSES_PER_ROT = 200;
 const int PULSES_PER_ROT = FULL_PULSES_PER_ROT * FACTOR / 2;
 const int ACCELERATION = PULSES_PER_ROT * 2;
 const int MAX_SPEED = PULSES_PER_ROT * 8;
-const int TEST_SPEED = PULSES_PER_ROT * 4;
+const int TEST_SPEED = PULSES_PER_ROT * 2;
 const int MIN_PULSE_WIDTH = 100;
 const int HOMING_START_POSITION = 0;
+
+bool constant_speed = false;
 
 void setupSteppers() {
   for (int i = 0; i < NUM_STEPPERS; ++i) {    
@@ -29,15 +31,27 @@ void setupSteppers() {
   }
 }
 
-void runAllSteppers() {
+void runAllSteppers(bool constant_speed = true) {
   for (int i = 0; i < NUM_STEPPERS; ++i) {
-    steppers[i].runSpeed();
+    if (constant_speed) {
+      steppers[i].runSpeed(); 
+    } else {
+      steppers[i].run();
+    }
   }
 }
 
 void stopAllSteppers() {
   for (int i = 0; i < NUM_STEPPERS; ++i) {
-    steppers[i].stop();
+     steppers[i].setSpeed(0);
+     steppers[i].runSpeed();
+  }
+}
+
+void homeAllSteppers() {
+  for (int i = 0; i < NUM_STEPPERS; ++i) {
+     steppers[i].moveTo(HOMING_START_POSITION);
+     steppers[i].setSpeed(TEST_SPEED);
   }
 }
 
@@ -50,21 +64,30 @@ void printSpeeds() {
 void setup() {
   setupSteppers();
   Serial.begin(9600);
-  Serial.println("initialized");
+  Serial.println("initialized - use PUTTY for smooth control!");
 }
 
-void loop() {  
+void loop() {
   if (Serial.available()) {
     char cmd = Serial.read();
     switch (cmd) {
       case '0': stopAllSteppers(); break;
-      case 'w': steppers[0].setSpeed(steppers[0].speed() + 100 * FACTOR); break;
-      case 's': steppers[0].setSpeed(steppers[0].speed() - 100 * FACTOR); break;
-      case 'o': steppers[1].setSpeed(steppers[1].speed() + 100 * FACTOR); break;
-      case 'l': steppers[1].setSpeed(steppers[1].speed() - 100 * FACTOR); break;
+      case 'h': stopAllSteppers(); homeAllSteppers(); break;
+      // big steps
+      case 'W': steppers[0].setSpeed(steppers[0].speed() + 100 * FACTOR); break;
+      case 'S': steppers[0].setSpeed(steppers[0].speed() - 100 * FACTOR); break;
+      case 'O': steppers[1].setSpeed(steppers[1].speed() + 100 * FACTOR); break;
+      case 'L': steppers[1].setSpeed(steppers[1].speed() - 100 * FACTOR); break;
+      // small steps
+      case 'w': steppers[0].setSpeed(steppers[0].speed() + 10 * FACTOR); break;
+      case 's': steppers[0].setSpeed(steppers[0].speed() - 10 * FACTOR); break;
+      case 'o': steppers[1].setSpeed(steppers[1].speed() + 10 * FACTOR); break;
+      case 'l': steppers[1].setSpeed(steppers[1].speed() - 10 * FACTOR); break;
       default: break;
     }
+    bool homing = (cmd == 'h');
+    constant_speed = !homing;
     printSpeeds();
   }
-  runAllSteppers();
+  runAllSteppers(constant_speed);
 }
